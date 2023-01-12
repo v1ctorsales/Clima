1fce1997efff2d7458d8e5574357b3f84b8269e5
Victor <victor.alves.sales@hotmail.com>
2023-01-11 21:43:12 -0300

    3

--- src/app/Components/weather-widget-main/weather-widget-main.component.ts
+++ src/app/Components/weather-widget-main/weather-widget-main.component.ts
@@ -8,6 +8,8 @@ import * as moment from 'moment';
 })
 export class WeatherWidgetMainComponent implements OnInit {
   WeatherData:any;
+  currenttime = new Date();
+  cityName?: string;
   constructor() {}
 
   ngOnInit() {
@@ -15,6 +17,13 @@ export class WeatherWidgetMainComponent implements OnInit {
     console.log(this.WeatherData)
 
   }
+
+  onSubmit(){
+      let data =  JSON.parse('{"coord":{"lon":151.2073,"lat":-33.8679},"weather":[{"id":803,"main":"Clouds","description":"broken clouds","icon":"04d"}],"base":"stations","main":{"temp":296.9,"feels_like":297,"temp_min":296.13,"temp_max":298.84,"pressure":1014,"humidity":64},"visibility":10000,"wind":{"speed":1.79,"deg":61,"gust":4.02},"clouds":{"all":59},"dt":1673404232,"sys":{"type":2,"id":2002865,"country":"AU","sunrise":1673376924,"sunset":1673428181},"timezone":39600,"id":2147714,"name":"Sydney","cod":200}')
+    this.setWeatherData(data);
+
+  }
+
   getWeatherData(){
     let data =  JSON.parse('{"coord":{"lon":151.2073,"lat":-33.8679},"weather":[{"id":803,"main":"Clouds","description":"broken clouds","icon":"04d"}],"base":"stations","main":{"temp":296.9,"feels_like":297,"temp_min":296.13,"temp_max":298.84,"pressure":1014,"humidity":64},"visibility":10000,"wind":{"speed":1.79,"deg":61,"gust":4.02},"clouds":{"all":59},"dt":1673404232,"sys":{"type":2,"id":2002865,"country":"AU","sunrise":1673376924,"sunset":1673428181},"timezone":39600,"id":2147714,"name":"Sydney","cod":200}')
     this.setWeatherData(data);
@@ -22,6 +31,7 @@ export class WeatherWidgetMainComponent implements OnInit {
 
   setWeatherData(data: any){
     this.WeatherData = data;
+    let country = new Date(this.WeatherData.sys.country);
     let sunsetRise = new Date(this.WeatherData.sys.sunrise * 1000);
     this.WeatherData.sunrise_time = sunsetRise.toLocaleTimeString();
     let sunsetTime = new Date(this.WeatherData.sys.sunset * 1000);
@@ -32,8 +42,5 @@ export class WeatherWidgetMainComponent implements OnInit {
     this.WeatherData.temp_min = (this.WeatherData.main.temp_min - 273.15).toFixed(0);
     this.WeatherData.temp_max = (this.WeatherData.main.temp_max - 273.15).toFixed(0);
     this.WeatherData.temp_feels_like = (this.WeatherData.main.feels_like - 273.15).toFixed(0);
-    console.log(this.WeatherData.timezone);
-    const horagora = new Date((this.WeatherData.sys.sunrise + (this.WeatherData.timezone)) * 1000)
-    console.log(horagora);
   }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,25 +10,4 @@ export class AppComponent {
   WeatherData:any;
   constructor() {}
 
-  ngOnInit() {
-    this.getWeatherData();
-    console.log(this.WeatherData)
-
-  }
-  getWeatherData(){
-    let data =  JSON.parse('{"coord":{"lon":-0.1257,"lat":51.5085},"weather":[{"id":500,"main":"Rain","description":"light rain","icon":"10n"}],"base":"stations","main":{"temp":285.37,"feels_like":285.05,"temp_min":284.32,"temp_max":286.13,"pressure":1001,"humidity":92},"visibility":10000,"wind":{"speed":6.69,"deg":220},"rain":{"1h":0.15},"clouds":{"all":100},"dt":1673382337,"sys":{"type":2,"id":2075535,"country":"GB","sunrise":1673337790,"sunset":1673367127},"timezone":0,"id":2643743,"name":"London","cod":200}')
-    this.setWeatherData(data);
-  }
-
-  setWeatherData(data: any){
-    this.WeatherData = data;
-    let sunsetTime = new Date(this.WeatherData.sys.sunset * 1000);
-    this.WeatherData.sunset_time = sunsetTime.toLocaleTimeString();
-    let currentDate = new Date();
-    this.WeatherData.isDay = (currentDate.getTime() < sunsetTime.getTime());
-    this.WeatherData.temp_celcius = (this.WeatherData.main.temp - 273.15).toFixed(0);
-    this.WeatherData.temp_min = (this.WeatherData.main.temp_min - 273.15).toFixed(0);
-    this.WeatherData.temp_max = (this.WeatherData.main.temp_max - 273.15).toFixed(0);
-    this.WeatherData.temp_feels_like = (this.WeatherData.main.feels_like - 273.15).toFixed(0);
-  }
 }
