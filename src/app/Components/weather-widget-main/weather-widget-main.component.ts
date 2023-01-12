9217ae08fe2641820be58082d5359bf0e01a7aed
Victor <victor.alves.sales@hotmail.com>
2023-01-12 00:40:09 -0300

    4

--- src/app/Components/weather-widget-main/weather-widget-main.component.ts
+++ src/app/Components/weather-widget-main/weather-widget-main.component.ts
@@ -9,7 +9,7 @@ import * as moment from 'moment';
 export class WeatherWidgetMainComponent implements OnInit {
   WeatherData:any;
   currenttime = new Date();
-  cityName?: string;
+  cityName: string = 'Contagem';
   constructor() {}
 
   ngOnInit() {
@@ -19,18 +19,28 @@ export class WeatherWidgetMainComponent implements OnInit {
   }
 
   onSubmit(){
-      let data =  JSON.parse('{"coord":{"lon":151.2073,"lat":-33.8679},"weather":[{"id":803,"main":"Clouds","description":"broken clouds","icon":"04d"}],"base":"stations","main":{"temp":296.9,"feels_like":297,"temp_min":296.13,"temp_max":298.84,"pressure":1014,"humidity":64},"visibility":10000,"wind":{"speed":1.79,"deg":61,"gust":4.02},"clouds":{"all":59},"dt":1673404232,"sys":{"type":2,"id":2002865,"country":"AU","sunrise":1673376924,"sunset":1673428181},"timezone":39600,"id":2147714,"name":"Sydney","cod":200}')
-    this.setWeatherData(data);
+      //this.getWeatherData(this.cityName);
+      this.cityName = '';
 
   }
 
-  getWeatherData(){
+  private getWeatherData(){//cityName: string
+    let url = ('https://api.openweathermap.org/data/2.5/weather?q='+'new york'+'&appid=01661b82dd64746b6ac02bf1fdebd38a')
+
+  fetch(url)
+  .then(res => res.json())
+  .then(out =>
+    this.setWeatherData(out))
+  .catch(err => { throw err })
+  
+  ;
+  
     let data =  JSON.parse('{"coord":{"lon":151.2073,"lat":-33.8679},"weather":[{"id":803,"main":"Clouds","description":"broken clouds","icon":"04d"}],"base":"stations","main":{"temp":296.9,"feels_like":297,"temp_min":296.13,"temp_max":298.84,"pressure":1014,"humidity":64},"visibility":10000,"wind":{"speed":1.79,"deg":61,"gust":4.02},"clouds":{"all":59},"dt":1673404232,"sys":{"type":2,"id":2002865,"country":"AU","sunrise":1673376924,"sunset":1673428181},"timezone":39600,"id":2147714,"name":"Sydney","cod":200}')
-    this.setWeatherData(data);
+
   }
 
-  setWeatherData(data: any){
-    this.WeatherData = data;
+  setWeatherData(url: any){
+    this.WeatherData = url;
     let country = new Date(this.WeatherData.sys.country);
     let sunsetRise = new Date(this.WeatherData.sys.sunrise * 1000);
     this.WeatherData.sunrise_time = sunsetRise.toLocaleTimeString();
