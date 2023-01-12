5afb278e2e5cb9ddbb91f64e8234aa3bbf85459d
Victor <victor.alves.sales@hotmail.com>
2023-01-12 19:37:51 -0300

    Primeiro commit funcional

--- src/app/Components/weather-widget-main/weather-widget-main.component.ts
+++ src/app/Components/weather-widget-main/weather-widget-main.component.ts
@@ -13,19 +13,19 @@ export class WeatherWidgetMainComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {
-    this.getWeatherData();
-    console.log(this.WeatherData)
+    this.getWeatherData(this.cityName);
+    this.cityName = '';
 
   }
 
   onSubmit(){
-      //this.getWeatherData(this.cityName);
+      this.getWeatherData(this.cityName);
       this.cityName = '';
 
   }
 
-  private getWeatherData(){//cityName: string
-    let url = ('https://api.openweathermap.org/data/2.5/weather?q='+'new york'+'&appid=01661b82dd64746b6ac02bf1fdebd38a')
+  private getWeatherData(cityName: string){//cityName: string
+    let url = ('https://api.openweathermap.org/data/2.5/weather?q='+cityName+'&appid=01661b82dd64746b6ac02bf1fdebd38a')
 
   fetch(url)
   .then(res => res.json())
@@ -34,8 +34,6 @@ export class WeatherWidgetMainComponent implements OnInit {
   .catch(err => { throw err })
   
   ;
-  
-    let data =  JSON.parse('{"coord":{"lon":151.2073,"lat":-33.8679},"weather":[{"id":803,"main":"Clouds","description":"broken clouds","icon":"04d"}],"base":"stations","main":{"temp":296.9,"feels_like":297,"temp_min":296.13,"temp_max":298.84,"pressure":1014,"humidity":64},"visibility":10000,"wind":{"speed":1.79,"deg":61,"gust":4.02},"clouds":{"all":59},"dt":1673404232,"sys":{"type":2,"id":2002865,"country":"AU","sunrise":1673376924,"sunset":1673428181},"timezone":39600,"id":2147714,"name":"Sydney","cod":200}')
 
   }
 
